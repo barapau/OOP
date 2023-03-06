6ebb21916b9aba59278c357d236afaa4f928130e
Paula <paula_baranano1@baylor.edu>
2023-03-05 18:04:40 -0600

    hmw

--- JoeProgram.py
+++ JoeProgram.py
@@ -9,6 +9,14 @@ year = 2016
 parts = 1,210.50
 labor = 765
 
-car = jc.Car(make, model, year)
 customer = jc.Customer(name, address, phone)
-service_quota = (parts, labor)
+car = jc.Car(make, model, year)
+#service_quota = (parts, labor)
+
+print('Customer:', customer.get.name(), '   ', 'Address:', customer.get.address(), '   ', 'Phone:', customer.get.phone())
+print('Car Make:', car.get.make(), '   ', 'Car Model:', customer.get.model(), '   ', 'Car year:', car.get.year())
+print('Service Quote')
+print('Parts:')
+print('Labor:')
+print('Sales Tax:')
+print('Total Charges:')
