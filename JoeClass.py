56a2727ee4e97aa3c8a269424ce607713a00735c
Paula <paula_baranano1@baylor.edu>
2023-03-05 18:04:33 -0600

    hmw

--- JoeClass.py
+++ JoeClass.py
@@ -34,17 +34,36 @@ class Car:
 #service quote class
 class ServiceQuote:
     def __init__(self,part_charges, labor_charges):
-        self.__part_charges = part_charges
-        self.__labor_charges= labor_charges
+        self.part_charges = part_charges
+        self.labor_charges= labor_charges
     
     def get_parts_charges(self):
-        return self.__part_charges
+        return self.part_charges
 
     def get_labor_charges(self):
-        return self.__labor_charges
+        return self.labor_charges
     
-    def set_model(self):
-        return self.__model
+    def get_tax(self):
+        total = self.part_charges + self.labor_charges
+        tax = total * 0.825
+        return tax
+    
+    def get_total(self):
+        total = self.part_charges + self.labor_charges
+        tax = total * 0.825
+        total1 = self.part_charges + self.labor_charges + tax
+        return total1
+    
+    def get_tax(self):
+        return self.tax
+    
+    def get_charges(self):
+        return self.get_charges
+    
+    def get_sales_tax(self):
+        return self.parts * .0825
+
+
     
     def set_year(self):
         return self.__year
