a2d8778980c811e9a090aa3696a147589331eda0
Paula <paula_baranano1@baylor.edu>
2023-03-05 20:54:30 -0600

    hmw

--- JoeProgram.py
+++ JoeProgram.py
@@ -6,17 +6,17 @@ phone = 214-555-1112
 make = 'Honda'
 model = 'Accord LX'
 year = 2016
-parts = 1,210.50
+parts = 1210.50
 labor = 765
 
 customer = jc.Customer(name, address, phone)
 car = jc.Car(make, model, year)
-#service_quota = (parts, labor)
+service_quote = jc.ServiceQuote(parts, labor)
 
-print('Customer:', customer.get.name(), '   ', 'Address:', customer.get.address(), '   ', 'Phone:', customer.get.phone())
-print('Car Make:', car.get.make(), '   ', 'Car Model:', customer.get.model(), '   ', 'Car year:', car.get.year())
+print('Customer:', customer.get_name(), '   ', 'Address:', customer.get_address(), '   ', 'Phone:', customer.get_phone())
+print('Car Make:', car.get_make(), '   ', 'Car Model:', car.get_model(), '   ', 'Car year:', car.get_year())
 print('Service Quote')
-print('Parts:')
-print('Labor:')
-print('Sales Tax:')
-print('Total Charges:')
+print('Parts:' '$', format(service_quote.get_parts_charges(), ',.2f'), sep ='')
+print('Labor:''$', format(service_quote.get_labor_charges(), ',.2f'), sep ='')
+print('Sales Tax:',format(service_quote.get_tax(), ',.2f'), sep ='')
+print('Total Charges:', format(service_quote.get_total(), ',.2f'), sep ='')
